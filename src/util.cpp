4e7391cfb1e237c3f8b07a1f0c7abda693ab5ec9
Yannick Schaffner <schyanni@ethz.ch>
2020-04-04 21:42:46 +0200

    Add sparsematrix and eq 3 of add. file2

---
+++ src/util.cpp
@@ -0,0 +1,58 @@
+#include "util.hpp"
+
+
+#include <math.h>
+
+namespace ABM
+{
+
+    SparseMatrix::SparseMatrix(std::vector<std::tuple<size_t, size_t, double>>& tripel) 
+        : Values(std::vector<double>()), ColumnIdx(std::vector<size_t>()), RowIdx(std::vector<size_t>())
+        {
+            std::sort(tripel.begin(), tripel.end(), [](std::tuple<size_t, size_t, double> a, std::tuple<size_t, size_t, double> b) { 
+                if (std::get<0>(a) < std::get<0>(b))
+                {
+                    return true;
+                } else if (std::get<0>(a) == std::get<0>(b))
+                {
+                    return std::get<1>(a) < std::get<1>(b);
+                }
+                return false;
+            });
+
+            Values.reserve(tripel.size());
+            RowIdx.push_back(0);
+            size_t currentRow = 0;
+            for(auto tpl : tripel)
+            {
+                while(std::get<0>(tpl) > currentRow)
+                {
+                    RowIdx.push_back( Values.size());
+                    currentRow++;
+                }
+
+                Values.push_back( std::get<2>(tpl));
+                ColumnIdx.push_back( std::get<1>(tpl));
+            }
+            RowIdx.push_back( Values.size());
+        }
+
+    double SparseMatrix::At(const size_t row, const size_t col) const
+    {    
+        // todo: implement nice binary search
+        for(size_t start = RowIdx[row]; start < RowIdx[row + 1]; start++)
+        {
+            if( ColumnIdx[start] == col)
+            {
+                return Values[start];
+            }
+        }
+        return 0.;
+    }
+
+    double Dist(const std::pair<double,double> locA, const std::pair<double,double> locB, const double param_a, const double param_b)
+    {
+        double dist = std::sqrt( std::pow( locA.first - locB.first, 2) + std::pow(locA.second - locB.second, 2) );
+        return 1./(1. + std::pow( dist/param_a, param_b) );
+    }
+}
---
+++ src/util.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <math.h>
+
+#pragma once
+
+
+
+namespace ABM
+{
+    struct SparseMatrix
+    {
+        std::vector<double> Values;
+        std::vector<size_t> ColumnIdx;
+        std::vector<size_t> RowIdx;
+
+        // the tuple is (row idx, col idx, value)
+        // the tuple-vector is not required to be sorted.
+        SparseMatrix(std::vector<std::tuple<size_t, size_t, double>>& tripel); 
+
+        // returns the value or 0
+        double At(size_t row, size_t col) const;
+
+    };
+
+    // computes f(d_ik) according to eq (3) in add. file
+    double Dist(const std::pair<double,double> locA, const std::pair<double,double> locB, const double param_a, const double param_b);
+}
